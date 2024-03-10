eaca633cd5e7e4d2d77b1e6c37974c35c4011d57
Fer Baeza <fbaezahurtado@gmail.com>
2024-03-10 15:51:27 +0000

    Custom Base Entity Collection

--- src/Shared/Laravel/Exceptions/CustomExceptions/CustomCollectionExceptions.php
+++ src/Shared/Laravel/Exceptions/CustomExceptions/CustomCollectionExceptions.php
@@ -4,7 +4,6 @@ namespace Src\Shared\Laravel\Exceptions\CustomExceptions;
 
 use Src\Shared\Laravel\Exceptions\CustomExceptions\BaseExceptions;
 
-
 class CustomCollectionExceptions extends BaseExceptions
 {
     const MSG = 'You can only add items of type %s to this collection.';
