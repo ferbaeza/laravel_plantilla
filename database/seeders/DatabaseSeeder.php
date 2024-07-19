d3216efeda42ee15e959ee975a088394d0dc8c18
Fer Baeza <fbaezahurtado@gmail.com>
2024-07-19 08:53:35 +0000

    Seeders Iniciales

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Database\Seeders\TestingSeeder;
+use Illuminate\Support\Facades\Schema;
+use Database\Seeders\TruncateAllTablesSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +15,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $this->command->info('Comienza a ejecutarse el DatabaseSeeder:');
+        DB::beginTransaction();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        Schema::disableForeignKeyConstraints();
+
+        $this->call([
+            TruncateAllTablesSeeder::class,
+            TestingSeeder::class,
         ]);
+
+        Schema::enableForeignKeyConstraints();
+        DB::commit();
     }
 }
---
+++ database/seeders/TestingSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TestingSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+
+        User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+        ]);
+    }
+}
